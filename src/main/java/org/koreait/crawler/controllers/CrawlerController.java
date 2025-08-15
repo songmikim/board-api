86000401702f26a3bde3bfaa33c18c8f7ca2fa87
songmikim <50817231+songmikim@users.noreply.github.com>
2025-08-15 19:25:03 +0900

    .

---
+++ src/main/java/org/koreait/crawler/controllers/CrawlerController.java
@@ -0,0 +1,29 @@
+package org.koreait.crawler.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.koreait.crawler.entities.CrawledData;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/crawler")
+@Tag(name="Crawler API")
+public class CrawlerController {
+
+    private final HttpServletRequest request;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping(method = {RequestMethod.PATCH, RequestMethod.POST})
+    public CrawledData update() {
+        String mode = request.getMethod().equalsIgnoreCase("PATCH") ? "update" : "register";
+
+        return null;
+    }
+
+}
---
+++ src/test/java/org/koreait/Crawler/services/CrawlingserviceTest.java
@@ -0,0 +1,27 @@
+package org.koreait.Crawler.services;
+
+import org.junit.jupiter.api.Test;
+import org.koreait.crawler.controllers.RequestCrawling;
+import org.koreait.crawler.services.CrawlingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CrawlingserviceTest {
+    @Autowired
+    private CrawlingService service;
+
+    @Test
+    void crawlingTest() {
+        RequestCrawling form = new RequestCrawling();
+        form.setUrl("https://www.me.go.kr/mamo/web/index.do?menuId=631");
+        form.setKeywords(List.of("환경", "수도권"));
+        form.setLinkSelector(".brd_body .title a");
+        form.setTitleSelector(".board_view .board_tit");
+        form.setDateSelector(".board_view .createDate");
+        form.setUrlPrefix("https://www.me.go.kr");
+        service.process(form);
+    }
+}
